ff35347b54e4116f80c834e557abcbc47336e831
NeoModise1 <Gladneo90@gmail.com>
2026-09-04 07:41:39 -0700

    Add files via upload

---
+++ inserting values 5.sql
@@ -0,0 +1,42 @@
+INSERT INTO EVENTS
+(
+    EventName,
+    Description,
+    EventDate,
+    StartTime,
+    Venue,
+    City,
+    Province,
+    Status
+)
+VALUES
+(
+    'Cape Town Coastal Run',
+    'A scenic road race along the Cape Town coastline.',
+    '2026-10-18',
+    '07:00:00',
+    'Green Point Athletics Stadium',
+    'Cape Town',
+    'Western Cape',
+    'Scheduled'
+),
+(
+    'Johannesburg City Challenge',
+    'A city running event featuring short and medium-distance races.',
+    '2026-11-08',
+    '06:30:00',
+    'Mary Fitzgerald Square',
+    'Johannesburg',
+    'Gauteng',
+    'Scheduled'
+),
+(
+    'Durban Summer Run',
+    'A fast-paced running event along the Durban beachfront.',
+    '2026-12-06',
+    '06:00:00',
+    'Moses Mabhida Stadium',
+    'Durban',
+    'KwaZulu-Natal',
+    'Scheduled'
+);
